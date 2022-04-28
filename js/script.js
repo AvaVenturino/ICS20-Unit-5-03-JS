b69a1499af6b9b281957990431069faf18115dbc
Ava Venturino <99355031+AvaVenturino@users.noreply.github.com>
2022-04-28 17:30:40 +0000

    added space

--- js/script.js
+++ js/script.js
@@ -17,21 +17,19 @@ if (navigator.serviceWorker) {
  * Input
  */
 function myButtonClicked() {
-  const age17 = parseInt(document.getElementById("age17").value)
-  const age13 = parseInt(document.getElementById("age13").value)
-  const age5 =  parseInt(document.getElementById("age5").value)
+  const age17 = document.getElementById("age17").checked
+  const age13 = document.getElementById("age13").checked
+  const age5 =  document.getElementById("age5").checked
 
   // process
-  if (age17 == 17) {
+  console.log(age17)
+  if (age17 == true) {
     document.getElementById("answer").innerHTML = "You can see an R rated movie alone"
-    }
-    else if (age13 == 13) {
+  } else if (age13 == true) {
       document.getElementById("answer").innerHTML = "You can see an PG 13 rated movie alone"
-    }
-    else if (age5 == 5) {
+  } else if (age5 == true) {
       document.getElementById("answer").innerHTML = "You can see an G rated movie alone"
-    }
-    else {
+  } else {
     document.getElementById("answer").innerHTML = "Uh. You are too young for most things!"
   }
 }
